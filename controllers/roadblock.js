537a63443771bcaced53044fe13a0e64aa09fbdf
Wumbaloo <wil-et-sam@hotmail.fr>
2021-03-30 14:05:18 +0200

    feat(roadblocks): can now edit a roadblock and delete one

--- controllers/roadblock.js
+++ controllers/roadblock.js
@@ -1,6 +1,79 @@
 const Roadblock = require('../models/roadblock');
 const TekYear = require('../models/year');
 
+exports.editRoadblock = async (req, res) => {
+    const { id } = req.params;
+    const { newName, newCreditsNeeded, year } = req.body;
+
+    if (!newName || !newCreditsNeeded || !year)
+        return res.status(400).json({ body: "Bad request parameter." });
+    const tekYear = await TekYear.findOne({
+        where: {
+            year
+        }
+    });
+    if (!tekYear)
+        return res.status(404).json({ body: "Invalid Tek Year." });
+    const result = await Roadblock.findOne({
+        where: {
+            id
+        },
+    });
+    if (!result)
+        return res.status(404).json({ body: "No roadblock exists." });
+    result.name = newName;
+    result.credits_needed = newCreditsNeeded;
+    result.tekYearId = tekYear.id;
+    await result.save();
+    res.json({ success: true, body: "Successfully edited. "});
+};
+
+exports.deleteRoadblock = async (req, res) => {
+    const { id } = req.params;
+
+    if (!id)
+        return res.status(400).json({ body: "Bad request parameter." });
+    const result = await Roadblock.findOne({
+        where: {
+            id
+        },
+    });
+    if (!result)
+        return res.status(404).json({ body: "No roadblock exists." });
+    await result.destroy();
+    res.json({ success: true, body: "Successfully deleted. "});
+};
+
+exports.createRoadblock = async (req, res) => {
+    const { name, credits_needed, year } = req.body;
+
+    if (!name || !credits_needed || !year)
+        return res.status(400).json({ body: "Missing name or credits_needed or year." });
+    const tekYear = await TekYear.findOne({
+        where: {
+            year
+        }
+    });
+    if (!tekYear)
+        return res.status(404).json({ body: "Invalid Tek Year." });
+    const result = await Roadblock.findOne({
+        where: {
+            name,
+            credits_needed
+        }
+    });
+    if (result)
+        return res.status(409).json({ body: "Roadblock already exists." });
+    const roadblock = await Roadblock.create({
+        name,
+        credits_needed,
+        tekYearId: tekYear.id
+    });
+    if (!roadblock)
+        return res.status(422).json({ body: "Success but can't create the roadblock. Try again later." });
+    res.json({ success: true, body: roadblock.toJSON() });
+};
+
 exports.getRoadblocks = async (req, res) => {
     const result = await Roadblock.findAll({ include: [TekYear] });
 
--- routes/roadblock.js
+++ routes/roadblock.js
@@ -7,4 +7,10 @@ const roadblockController = require('../controllers/roadblock');
 
 router.get('/backoffice/roadblocks', middleware.checkAuth, roadblockController.getRoadblocks);
 
+router.post('/backoffice/roadblock', middleware.checkAuth, roadblockController.createRoadblock);
+
+router.post('/backoffice/roadblock/:id', middleware.checkAuth, roadblockController.editRoadblock);
+
+router.delete('/backoffice/roadblock/:id', middleware.checkAuth, roadblockController.deleteRoadblock);
+
 module.exports = router;
